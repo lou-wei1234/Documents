e0e72373ce306ba7ced2647fa1a8841fb8997919
lou-wei1234 <133888562+lou-wei1234@users.noreply.github.com>
2023-10-05 08:32:23 +0800

    Update s03.py

--- bcs24-otanes/s03/discussion/s03.py
+++ bcs24-otanes/s03/discussion/s03.py
@@ -1,1 +1,29 @@
+import os
+import time
 
+num_floors = 10
+target_floor = 5
+
+# initialize the array to contain "|____|" in all indices
+floors = ["|____|"] * num_floors
+
+for floor in range(num_floors):
+    # clear console
+    os.system('cls')
+
+    # update the position of the symbol
+    if floor > 0:
+        floors[floor - 1] = "|____|"
+        
+    floors[floor] = "|_🙂_|"
+    
+    # print floors
+    for i in range(num_floors, 0, -1):
+        print(floors[i - 1], i)
+    
+    # When destination is reached
+    if floor == target_floor - 1:
+        print("Destination reached!")
+        break
+    
+    time.sleep(0.5)
